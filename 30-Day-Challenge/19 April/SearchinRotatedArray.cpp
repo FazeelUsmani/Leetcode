b650382415fa93ac9bbe3ded843bca1e887b392b
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-04-19 23:34:25 +0530

    FB question using Binary Search TC: O(log N) time

---
+++ 30-Day-Challenge/19 April/SearchinRotatedArray.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low = 0, high = nums.size() - 1;
+        
+        while (low <= high){
+            int mid = low + (high - low)/2;
+            if (nums[mid] == target)
+                    return mid;
+            
+            if (nums[mid] <= nums[high]){
+                if (target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            if (nums[mid] >= nums[low]){
+                if (target < nums[mid] && target >= nums[low])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+        }
+        
+        return -1;
+        
+    }
+};
