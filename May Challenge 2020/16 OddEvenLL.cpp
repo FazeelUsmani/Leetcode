db99bebdea76e9cbe2280b557e540027f6051264
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-05-16 15:36:46 +0530

    2 solutions

--- May Challenge 2020/16 OddEvenLL.cpp
+++ May Challenge 2020/16 OddEvenLL.cpp
@@ -37,3 +37,55 @@ public:
         return head;
     }
 };
+
+
+
+
+
+
+
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//            Optimized Code:
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   
+ 
+   
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        
+        if (head == NULL || head->next == NULL || head->next->next == NULL)
+            return head;
+        
+        ListNode *prev = NULL, *h1 = head, *h2 = head->next, *ptrStart = h2;
+        
+        while (h1 != NULL && h2 != NULL){
+            
+            h1->next = h2->next;
+            prev = h1;
+            h1 = h1->next;
+            
+            if (h1 != NULL){
+                h2->next = h1->next;
+                h2 = h2->next;           
+            }   
+            else
+                prev->next = ptrStart;
+        }
+        
+        if (h1 != NULL)
+            h1->next = ptrStart;
+        
+        return head;
+    }
+};
