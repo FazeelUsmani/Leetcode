ce085237758a2306a82634d4739886f86e2ba1c9
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-15 12:41:52 +0530

    Create 15 searchInBST.cpp

---
+++ June Challenge 2020/15 searchInBST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        
+        while (root != NULL && root->val != val){
+            if (root->val < val)
+                root = root->right;
+            else
+                root = root->left;
+            
+        }
+        
+        return root;
+        
+    }
+};
