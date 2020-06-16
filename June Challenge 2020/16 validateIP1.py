9ab3c14f65618d633264d92c12ecf00a247396c2
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-17 00:49:30 +0530

    using RegEx

---
+++ June Challenge 2020/16 validateIP1.py
@@ -0,0 +1,9 @@
+# Read this article: https://leetcode.com/articles/validate-ip-address/
+
+from ipaddress import ip_address, IPv6Address
+class Solution:
+    def validIPAddress(self, IP: str) -> str:
+        try:
+            return "IPv6" if type(ip_address(IP)) is IPv6Address else "IPv4"
+        except ValueError:
+            return "Neither"
