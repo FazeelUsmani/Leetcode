a7cf2111bbc37025a9377fc40c30007a51982e6c
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-06-20 00:04:18 +0530

    Create 19 LongestDuplicateSubstring.cpp

---
+++ June Challenge 2020/19 LongestDuplicateSubstring.cpp
@@ -0,0 +1,26 @@
+class Solution(object):
+    def longestDupSubstring(self, S):
+        
+        A = [ord(c) - ord('a') for c in S]
+        mod = 2**63 - 1
+
+        def test(L):
+            p = pow(26, L, mod)
+            cur = reduce(lambda x, y: (x * 26 + y) % mod, A[:L], 0)
+            seen = {cur}
+            for i in xrange(L, len(S)):
+                cur = (cur * 26 + A[i] - A[i - L] * p) % mod
+                if cur in seen: return i - L + 1
+                seen.add(cur)
+        res, lo, hi = 0, 0, len(S)
+        while lo < hi:
+            mi = (lo + hi + 1) / 2
+            pos = test(mi)
+            if pos:
+                lo = mi
+                res = pos
+            else:
+                hi = mi - 1
+                
+        return S[res:res + lo]
+        
