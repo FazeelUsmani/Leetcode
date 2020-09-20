cb1523a032392bd20245dc3057d1f435b7e40f94
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-09-20 17:43:27 +0530

    Create 20 uniquePaths3.py

---
+++ September Challenge 2020/20 uniquePaths3.py
@@ -0,0 +1,54 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        # step 1). initialize the conditions for backtracking
+        #   i.e. initial state and final state
+        non_obstacles = 0
+        start_row, start_col = 0, 0
+        for row in range(0, rows):
+            for col in range(0, cols):
+                cell = grid[row][col] 
+                if  cell >= 0:
+                    non_obstacles += 1
+                if cell == 1:
+                    start_row, start_col = row, col
+
+        # count of paths as the final result
+        path_count = 0
+
+        # step 2). backtrack on the grid
+        def backtrack(row, col, remain):
+            # we need to modify this external variable
+            nonlocal path_count
+
+            # base case for the termination of backtracking
+            if grid[row][col] == 2 and remain == 1:
+                # reach the destination
+                path_count += 1
+                return
+
+            # mark the square as visited. case: 0, 1, 2 
+            temp = grid[row][col] 
+            grid[row][col] = -4
+            remain -= 1   # we now have one less square to visit
+
+            # explore the 4 potential directions around
+            for ro, co in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                next_row, next_col = row + ro, col + co
+
+                if not (0 <= next_row < rows and 0 <= next_col < cols):
+                    # invalid coordinate
+                    continue
+                if grid[next_row][next_col] < 0:
+                    # either obstacle or visited square
+                    continue
+
+                backtrack(next_row, next_col, remain)
+
+            # unmark the square after the visit
+            grid[row][col] = temp
+
+        backtrack(start_row, start_col, non_obstacles)
+
+        return path_count
