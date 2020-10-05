a30a9ee3f5a5faad5799922a9cbc900f7ba4ae15
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-05 23:20:57 +0530

    Create 05 complementBase10.cpp

---
+++ October Challenge 2020/05 complementBase10.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        
+        int X = 1;
+        while (N > X) X = X * 2 + 1;
+        
+        return X - N;        
+    }
+};
