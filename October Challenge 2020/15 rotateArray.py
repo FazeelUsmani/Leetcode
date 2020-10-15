aa50788ffe32c36ea359828ad0327d6384f82a29
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-10-15 12:56:20 +0530

    Create 15 rotateArray.py

---
+++ October Challenge 2020/15 rotateArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reverse(self, nums: list, start: int, end: int) -> None:
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start, end = start + 1, end - 1
+                
+    def rotate(self, nums: List[int], k: int) -> None:
+        n = len(nums)
+        k %= n
+
+        self.reverse(nums, 0, n - 1)
+        self.reverse(nums, 0, k - 1)
+        self.reverse(nums, k, n - 1)
