509fdd34d267e54facaa77aa9db34b7f61c480a7
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-11-16 23:40:39 +0530

    Create 15 rangeSumBST.py

---
+++ November Challenge 2020/15 rangeSumBST.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rangeSumBST(self, root, L, R):
+        def dfs(node):
+            if node:
+                if L <= node.val <= R:
+                    self.ans += node.val
+                if L < node.val:
+                    dfs(node.left)
+                if node.val < R:
+                    dfs(node.right)
+
+        self.ans = 0
+        dfs(root)
+        return self.ans
