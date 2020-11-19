c9b7241c89b34496fdc9519200cac819e34cb9f8
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-11-19 22:21:23 +0530

    Create 19 decodeString.py

---
+++ November Challenge 2020/19 decodeString.py
@@ -0,0 +1,20 @@
+      
+class Solution:
+    def decodeString(self, s):
+        it, num, stack = 0, 0, [""]
+        while it < len(s):
+            if s[it].isdigit():
+                num = num * 10 + int(s[it])
+            elif s[it] == "[":
+                stack.append(num)
+                num = 0
+                stack.append("")
+            elif s[it] == "]":
+                str1 = stack.pop()
+                rep = stack.pop()
+                str2 = stack.pop()
+                stack.append(str2 + str1 * rep)
+            else:
+                stack[-1] += s[it]              
+            it += 1           
+        return "".join(stack)
