b704e620f6458705cc5e4ffe85551932b71d45de
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-11-23 22:45:31 +0530

    Create 23 houseRobber3.py

---
+++ November Challenge 2020/23 houseRobber3.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rob(self, root: TreeNode) -> int:
+        def helper(node):
+            # return [rob this node, not rob this node]
+            if not node:
+                return (0, 0)
+            left = helper(node.left)
+            right = helper(node.right)
+            # if we rob this node, we cannot rob its children
+            rob = node.val + left[1] + right[1]
+            # else we could choose to either rob its children or not
+            not_rob = max(left) + max(right)
+            return [rob, not_rob]
+
+        return max(helper(root))
