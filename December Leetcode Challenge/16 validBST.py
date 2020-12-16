43f7363927e8244f3a086ade2d2d99cec01c47b3
Fazeel Usmani <fazeelusmani18@gmail.com>
2020-12-16 23:54:26 +0530

    Create 16 validBST.py

---
+++ December Leetcode Challenge/16 validBST.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        stack, prev = [], -math.inf
+        
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            # If next element in inorder traversal
+            # is smaller than the previous one
+            # that's not BST.
+            if root.val <= prev:
+                return False
+            prev = root.val
+            root = root.right
+
+        return True
+        
+        3
+        
