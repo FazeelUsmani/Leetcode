f0897f3feee62f5a523842afb32790b589261966
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-01-22 23:09:41 +0530

    using sets

---
+++ January 2021/22 determingTwoStringsAreClose.cpp
@@ -0,0 +1,4 @@
+class Solution:
+    def closeStrings(self, w1: str, w2: str) -> bool:
+        
+        return set(w1) == set(w2) and Counter(Counter(w1).values()) == Counter(Counter(w2).values())
