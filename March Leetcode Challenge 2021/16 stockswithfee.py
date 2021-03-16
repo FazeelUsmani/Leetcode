1ff2b368a1f1bc08bdba61898018ad29e5f7a44e
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-03-16 23:37:59 +0530

    Create 16 stockswithfee.py

---
+++ March Leetcode Challenge 2021/16 stockswithfee.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxProfit(self, prices, fee):
+        
+        cash, hold = 0, -prices[0]
+        for i in range(1, len(prices)):
+            cash = max(cash, hold + prices[i] - fee)
+            hold = max(hold, cash - prices[i])
+        return cash
