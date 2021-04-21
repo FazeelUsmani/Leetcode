769acc81d814bdcce523a212ab1d1a73363f10a5
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-04-21 23:01:23 +0530

    Create 21 Triangle.py

---
+++ April Leetcode Challenge 2021/21 Triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        @lru_cache(maxsize=None)
+        def min_path(row, col):
+            path = triangle[row][col]
+            if row < len(triangle) - 1:
+                path += min(min_path(row + 1, col), min_path(row + 1, col + 1))
+            return path
+        return min_path(0, 0)
