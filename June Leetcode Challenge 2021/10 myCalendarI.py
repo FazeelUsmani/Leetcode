53b97be23e39151aa1465e0f059054c3b609d85a
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-06-10 22:45:22 +0530

    Create 10 myCalendarI.py

---
+++ June Leetcode Challenge 2021/10 myCalendarI.py
@@ -0,0 +1,30 @@
+class Node:
+    __slots__ = 'start', 'end', 'left', 'right'
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        self.left = self.right = None
+
+    def insert(self, node):
+        if node.start >= self.end:
+            if not self.right:
+                self.right = node
+                return True
+            return self.right.insert(node)
+        elif node.end <= self.start:
+            if not self.left:
+                self.left = node
+                return True
+            return self.left.insert(node)
+        else:
+            return False
+
+class MyCalendar(object):
+    def __init__(self):
+        self.root = None
+
+    def book(self, start, end):
+        if self.root is None:
+            self.root = Node(start, end)
+            return True
+        return self.root.insert(Node(start, end))
