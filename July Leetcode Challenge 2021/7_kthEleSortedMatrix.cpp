0f15f428a98d64cf16d556fba101deaa47eaa2a7
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-07-07 22:36:24 +0530

    Create 7_kthEleSortedMatrix.cpp

---
+++ July Leetcode Challenge 2021/7_kthEleSortedMatrix.cpp
@@ -0,0 +1,24 @@
+class Solution { // 24 ms, faster than 95.29%
+public:
+    int m, n;
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+        m = matrix.size(), n = matrix[0].size(); // For general, the matrix need not be a square
+        int left = matrix[0][0], right = matrix[m-1][n-1], ans = -1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (countLessOrEqual(matrix, mid) >= k) {
+                ans = mid;
+                right = mid - 1;
+            } else left = mid + 1;
+        }
+        return ans;
+    }
+    int countLessOrEqual(vector<vector<int>>& matrix, int x) {
+        int cnt = 0, c = n - 1; // start with the rightmost column
+        for (int r = 0; r < m; ++r) {
+            while (c >= 0 && matrix[r][c] > x) --c;  // decrease column until matrix[r][c] <= x
+            cnt += (c + 1);
+        }
+        return cnt;
+    }
+};
