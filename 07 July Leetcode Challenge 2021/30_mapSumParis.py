22b8374e4a59655af2b1ee210b0b4878e6f00ec7
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-07-30 21:16:26 +0530

    Create 30_mapSumParis.py

---
+++ 07 July Leetcode Challenge 2021/30_mapSumParis.py
@@ -0,0 +1,33 @@
+class TrieNode(object):
+    __slots__ = 'children', 'score'
+    def __init__(self):
+        self.children = {}
+        self.score = 0
+
+class MapSum(object):
+    def __init__(self):
+        self.map = {}
+        self.root = TrieNode()
+
+    def insert(self, key, val):
+        delta = val - self.map.get(key, 0)
+        self.map[key] = val
+        cur = self.root
+        cur.score += delta
+        for char in key:
+            cur = cur.children.setdefault(char, TrieNode())
+            cur.score += delta
+
+    def sum(self, prefix):
+        cur = self.root
+        for char in prefix:
+            if char not in cur.children:
+                return 0
+            cur = cur.children[char]
+        return cur.score
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
