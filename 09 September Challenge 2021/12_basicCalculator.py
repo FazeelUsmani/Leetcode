6f559f7eb0a901c3044ee996465b21f0dff39642
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-09-12 00:05:57 +0530

    Create 12_basicCalculator.py

---
+++ 09 September Challenge 2021/12_basicCalculator.py
@@ -0,0 +1,28 @@
+class Solution:
+    def calculate(self, s):    
+        def calc(it):
+            def update(op, v):
+                if op == "+": stack.append(v)
+                if op == "-": stack.append(-v)
+                if op == "*": stack.append(stack.pop() * v)
+                if op == "/": stack.append(int(stack.pop() / v))
+        
+            num, stack, sign = 0, [], "+"
+            
+            while it < len(s):
+                if s[it].isdigit():
+                    num = num * 10 + int(s[it])
+                elif s[it] in "+-*/":
+                    update(sign, num)
+                    num, sign = 0, s[it]
+                elif s[it] == "(":
+                    num, j = calc(it + 1)
+                    it = j - 1
+                elif s[it] == ")":
+                    update(sign, num)
+                    return sum(stack), it + 1
+                it += 1
+            update(sign, num)
+            return sum(stack)
+
+        return calc(0)
