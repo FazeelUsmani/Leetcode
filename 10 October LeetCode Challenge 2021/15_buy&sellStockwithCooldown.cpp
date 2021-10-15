8564c41875356bd9bfce09f0baf4a7e9201987ee
Fazeel Usmani <fazeelusmani18@gmail.com>
2021-10-15 23:59:29 +0530

    Create 15_buy&sellStockwithCooldown.cpp

---
+++ 10 October LeetCode Challenge 2021/15_buy&sellStockwithCooldown.cpp
@@ -0,0 +1,10 @@
+int maxProfit(vector<int> &prices) {
+    int buy(INT_MIN), sell(0), prev_sell(0), prev_buy;
+    for (int price : prices) {
+        prev_buy = buy;
+        buy = max(prev_sell - price, buy);
+        prev_sell = sell;
+        sell = max(prev_buy + price, sell);
+    }
+    return sell;
+}
